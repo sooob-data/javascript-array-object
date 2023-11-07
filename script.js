cfb7830a854107b78faa9a18b2e2b29c0b9da48e
sooob-data <149668175+sooob-data@users.noreply.github.com>
2023-11-07 15:28:33 +0000

    practice completed

---
+++ script.js
@@ -0,0 +1,55 @@
+const students = [{
+name1 : "shubham",
+age1:22,
+location1:"mumbai",
+scholar1:false
+},
+{name2:"alok",
+age2:23,
+location2:"thane",
+scholar2:true
+},
+{
+    name3:"lakshay",
+    age3:22,
+    location3:"uttrakhand",
+    scholar3:true
+},
+{
+    name4:"siddhu",
+    age4:24,
+    location4:"mumbai",
+    scholar4:false
+},
+{
+    name5:"vikram",
+    age5:25,
+    location5:"uttarpradesh",
+    scholar5:true
+}
+];
+const student_name=students[0].name1;
+const student_age =students[0].age1;
+const student_loc = students[0].location1;
+const student_grade = students[0].scholar1
+console.log(`${student_name} is ${student_age} and he is from ${student_loc}. he is smart ${student_grade}`)
+const student_name1=students[1].name2;
+const student_age1 =students[1].age2;
+const student_loc1 = students[1].location2;
+const student_grade1 = students[1].scholar2
+console.log(`${student_name1} is ${student_age1} and he is from ${student_loc1}. he is to smart ${student_grade1}`)
+const student_name2=students[2].name3;
+const student_age2=students[2].age3;
+const student_loc2= students[2].location3;
+const student_grade2 = students[2].scholar3
+console.log(`${student_name2} is ${student_age2} and he is from ${student_loc2}. he is to smart ${student_grade2}`)
+const student_name3=students[3].name4;
+const student_age3 =students[3].age4;
+const student_loc3 = students[3].location4;
+const student_grade3 = students[3].scholar4
+console.log(`${student_name3} is ${student_age3} and he is from ${student_loc3}. he is to smart ${student_grade3}`)
+const student_name4=students[4].name5;
+const student_age4 =students[4].age5;
+const student_loc4 = students[4].location5;
+const student_grade4 = students[4].scholar5
+console.log(`${student_name4} is ${student_age4} and he is from ${student_loc4}. he is to smart ${student_grade4 }`)
